8544f3156702e49a9f4a2d6daf6f5984d99f9294
Yago Portela Silva de Vasconcelos <theypsv@gmail.com>
2024-02-09 19:21:12 -0300

    Create escrevendo_as_classes_de_um_jogo_yago.py

---
+++ escrevendo_as_classes_de_um_jogo_yago.py
@@ -0,0 +1,19 @@
+class Heroi:
+    def __init__(self, nome, idade, tipo):
+        self.nome = nome
+        self.idade = idade
+        self.tipo = tipo
+
+    def atacar(self):
+        if self.tipo == "mago":
+            ataque = "usou magia"
+        elif self.tipo == "guerreiro":
+            ataque = "usou espada"
+        elif self.tipo == "monge":
+            ataque = "usou artes marciais"
+        elif self.tipo == "ninja":
+            ataque = "usou shuriken"
+        else:
+            ataque = "não possui um ataque definido"
+
+        print(f"O {self.tipo} {self.nome} atacou usando {ataque}!!")
